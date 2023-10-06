f3cfa80db85ebc82060bedbcfc100d9fb4964bf4
ChetanChaudhary6 <chetanchaudhary015@gmail.com>
2023-10-06 13:55:19 +0530

    Commit message for submodule changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+import { BrowserRouter as Router, Route,Routes} from 'react-router-dom';
+import MyForm from './components/MyForm';
+import Login from './components/Login';
+import Homepage from './components/Homepage/Homepage';
+import Election from './components/Election/App';
+
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login/>} />
+        <Route path="/Homepage" element={<Homepage/>} />
+        <Route path="/Election" element={<Election/>} />
+        <Route path="/MyForm" element={<MyForm/>} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
